1636483a830c1c6ea6415746b75111db6083e736
bo.vermote <bo.vermote@student.howest.be>
2025-04-08 17:21:30 +0200

    Init create login + functional plaintextpwd to hash

--- VaultViewer/VaultViewer/ServiceLayer/LoginService.cs
+++ VaultViewer/VaultViewer/ServiceLayer/LoginService.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using MySql.Data.MySqlClient;
+using BCrypt.Net;
+using System.Security.Cryptography.X509Certificates;
+
 
 namespace VaultViewer.ServiceLayer
 { // should not know whether it's wpf/console/etc
     public class LoginService
     {
+        // db connection
         private string connectionString = "Server=localhost;Database=vaultviewer;Uid=root;Pwd=root";
 
        // check if connectionstring is valid or not.
@@ -32,6 +36,12 @@ namespace VaultViewer.ServiceLayer
             }
         }
 
+        // password hashing:
+        public string HashPassword(string plainPassword)
+        {
+            
+            return BCrypt.Net.BCrypt.HashPassword(plainPassword);
+        }
 
 
         public bool Authenticate(string username, string password)
@@ -39,32 +49,74 @@ namespace VaultViewer.ServiceLayer
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 conn.Open(); // don't forget to close
-                string query = "SELECT EmployeeID from employeelogin WHERE username = @username AND passwordhash = @passwordhash";
+                string query = "SELECT PasswordHash FROM employeelogin WHERE UserName = @username";
                 // employee --> employeeRole --> role 
                 using (MySqlCommand cmd = new MySqlCommand(query, conn))
                 {
                     cmd.Parameters.AddWithValue("@username", username);
-                    cmd.Parameters.AddWithValue("@passwordhash", password);
+                    //cmd.Parameters.AddWithValue("@passwordhash", password);
 
                     using (var reader = cmd.ExecuteReader())
                     {
+                        if (reader.Read())
+                        {
+                            string storedHash = reader.GetString("PasswordHash");
+                            return BCrypt.Net.BCrypt.Verify(password, storedHash); // also return list of roles
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        // Create new user & convert plaintext pwd into hashed pwd
+        public bool CreateUser(string username, string password)
+        {
+            string hashedPassword = HashPassword(password);
 
-                        return reader.HasRows; // also return list of roles
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Optional: Check if user already exists
+                string checkQuery = "SELECT COUNT(*) FROM employeelogin WHERE UserName = @username";
+                using (MySqlCommand checkCmd = new MySqlCommand(checkQuery, conn))
+                {
+                    checkCmd.Parameters.AddWithValue("@username", username);
+                    int userExists = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                    if (userExists > 0)
+                    {
+                        // Username already exists
+                        return false;
                     }
                 }
 
-/* SELECT r.name
-FROM
-    Users u JOIN
-    Employee e ON u.EmployeeId = e.EmployeeId JOIN
-    EmployeeRole er ON e.EmployeeId = er.EmployeeId JOIN
-    Role r ON er.RoleId = r.RoleId
-WHERE
-    u.UserName = @UserName AND
-    u.Password = @Password;
-*/
+                string insertQuery = "INSERT INTO employeelogin (UserName, PasswordHash) VALUES (@username, @passwordhash)";
+                using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@passwordhash", hashedPassword);
 
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
             }
         }
+
+
+        /* SELECT r.name
+        FROM
+            Users u JOIN
+            Employee e ON u.EmployeeId = e.EmployeeId JOIN
+            EmployeeRole er ON e.EmployeeId = er.EmployeeId JOIN
+            Role r ON er.RoleId = r.RoleId
+        WHERE
+            u.UserName = @UserName AND
+            u.Password = @Password;
+        */
+
     }
 }
--- VaultViewer/VaultViewer/UI/MainWindow.xaml.cs
+++ VaultViewer/VaultViewer/UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,14 @@ namespace VaultViewer.UI
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
+        private void hashing()
+        {
+            var database = new LoginService();
+            string hash = database.HashPassword("Password1");
+        }
         private void BtnTestConnection_Click(object sender, RoutedEventArgs e) // click <-- event | sender = what created the event | routedeventargs <-- info of clicker (like role) (rarely used)
         {
             var a = Elliot.ActualHeight;
@@ -34,21 +40,41 @@ namespace VaultViewer.UI
         private void BtnAuthenticate(object sender, RoutedEventArgs e)
         {
             var a = Authenticate.ActualHeight;
+
             var database = new LoginService();
             string Username = txt_Username.Text;
-            string Password = txt_Password.Text;
-            database.Authenticate(Username, Password);
+            string Password = txt_Password.Password;
+
             bool success = database.Authenticate(Username, Password);
 
             if (success)
             {
-              MessageBox.Show("Login succesfull : D");
-             }
+                MessageBox.Show("Login succesfull : D");
+            }
             else
             {
-              MessageBox.Show("Invalid login details : (.");
-             }
+                MessageBox.Show("Invalid login details : (");
+            }
         }
+        private void BtnCreateUser(object sender, RoutedEventArgs e)
+        {
+            var database = new LoginService();
+            string Username = txt_input_Username.Text;
+            string Password = txt_input_Password.Password;
+            bool success = database.CreateUser(Username, Password);
+
+            if (success)
+            {
+                MessageBox.Show("User created succesfully : D");
+            }
+            else
+            {
+                MessageBox.Show("User creation unsuccesfull :(");
+            }
+        }
+    }
+}
+
         // Buttons are instantiated or not depending on role of user
         // Make second window LoginWindow --> MainWindow
 
@@ -56,5 +82,4 @@ namespace VaultViewer.UI
 
 
 
-    }
-}
+    
